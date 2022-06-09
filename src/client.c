b745ca29b582da5c2d0ccfc913bc32e7ce07199a
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-06-09 14:05:39 -0300

    cadastro de cliente

--- src/client.c
+++ src/client.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 
 typedef struct
 {
     int cod;
-    char name[100];
-    char address[100];
+    char *name;
+    char *address;
 
     int (*mediaValores)(int, int, int);
 } Client;
@@ -17,9 +16,12 @@ int Client_MediaValores(int v1, int v2, int v3);
 void NewClient(Client *c, int cod, char *name, char *address)
 {
     c->cod = cod;
-    strcpy(c->name, name);
-    strcpy(c->address, address);
+    // TODO: strcpy fica dando warning
+    // strcpy(c->name, name);
+    // strcpy(c->address, address);
 
+    c->name = name;
+    c->address = address;
     c->mediaValores = Client_MediaValores;
 }
 
--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
 #include "client.c"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int lastClientIdx = -1;
+Client clients[100] = {};
+
+void registerClient();
 
 // programa principal
 int main(void)
 {
-    Client client;
-    NewClient(&client, 1, "Cliente 1", "rua da saudade 123");
-
-    int res;
+    registerClient();
+    registerClient();
+    registerClient();
     printf("ok");
 
     // // popula objeto
@@ -21,4 +27,28 @@ int main(void)
     // // chama metodo do objeto
     // res = client.mediaValores(client.Valor1, client.Valor2, client.Valor3);
     // printf("\n\rResultado do metodo: %d\n\n", res);
+}
+
+void registerClient()
+{
+    const size_t maxStrLength = 100;
+    char *name = malloc(maxStrLength);
+    char *address = malloc(maxStrLength);
+    printf("Por favor, informe o nome do cliente: ");
+    fgets(name, maxStrLength, stdin);
+    printf("Por favor, informe o endereço do cliente: ");
+    fgets(address, maxStrLength, stdin);
+
+    /* Remove trailing newline, if there. */
+    if ((strlen(name) > 0) && (name[strlen(name) - 1] == '\n'))
+        name[strlen(name) - 1] = '\0';
+    if ((strlen(address) > 0) && (address[strlen(address) - 1] == '\n'))
+        address[strlen(address) - 1] = '\0';
+
+    lastClientIdx++;
+    const int clientCod = lastClientIdx;
+    Client client;
+    NewClient(&client, clientCod, name, address);
+    clients[lastClientIdx] = client;
+    printf("Cliente %s cadastrado. Cod do cliente: %d\n", client.name, client.cod);
 }
