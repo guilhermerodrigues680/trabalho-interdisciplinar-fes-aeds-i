91ba7fe333c61704d2edf45e86503d83ba2983dd
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-06-10 16:59:30 -0300

    leitura e impressão de data

--- src/location.h
+++ src/location.h
@@ -1,9 +1,11 @@
 #ifndef HEADER_LOCATION_H
 #define HEADER_LOCATION_H
 
+#include <time.h>
+
 typedef struct
 {
-    void (*registerLocation)(int clientCod, int vehicleCod);
+    void (*registerLocation)(int clientCod, int vehicleCod, struct tm withdrawalDate, struct tm returnDate);
     // void (*listLocations)(void);
 } LocationRepo;
 
--- src/service.c
+++ src/service.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
 #include "service.h"
 #include "client.h"
 #include "location.h"
 #include "vehicle.h"
+
+int readDateFromStdin(struct tm *date);
+formatDate(struct tm *date, char *buffer, size_t sizeBuffer);
 
 void serviceRegisterClient()
 {
@@ -28,8 +33,47 @@ void serviceRegisterLocation()
         printf("Não foi encontrado o cliente com o código informado\n");
         return;
     }
-
     printf("Cliente %s\n", c.name);
+
+    printf("Por favor, informe a data da retirada no formato \"dd/mm/aaaa hh:mm\": ");
+    struct tm withdrawalDate;
+    if (!readDateFromStdin(&withdrawalDate))
+    {
+        printf("A data inserida é inválida\n");
+        return;
+    }
+
+    printf("Por favor, informe a data de devolução no formato \"dd/mm/aaaa hh:mm\": ");
+    struct tm returnDate;
+    if (!readDateFromStdin(&returnDate))
+    {
+        printf("A data inserida é inválida\n");
+        return;
+    }
+
+    char bufFmtWithdrawalDate[20];
+    char bufFmtReturnDate[20];
+    formatDate(&withdrawalDate, bufFmtWithdrawalDate, sizeof(bufFmtWithdrawalDate));
+    formatDate(&returnDate, bufFmtReturnDate, sizeof(bufFmtReturnDate));
+    printf("Periodo de locação: %s -> %s\n", bufFmtWithdrawalDate, bufFmtReturnDate);
+
+    time_t epochWithdrawalDate = mktime(&withdrawalDate);
+    time_t epochReturnDate = mktime(&returnDate);
+
+    // Compara as datas, definido o horário para 00:00
+    const int secsInDay = (60 * 60 * 24);
+    const time_t epochWithdrawalDate0000 = epochWithdrawalDate - (epochWithdrawalDate % secsInDay);
+    const time_t epochReturnDate0000 = epochReturnDate - (epochReturnDate % secsInDay);
+    if (epochWithdrawalDate0000 > epochReturnDate0000)
+    {
+        printf("Erro: A data de retorno deve ser após a data de retirada\n");
+        return;
+    }
+    else if (epochWithdrawalDate == epochReturnDate)
+    {
+        printf("Retirada e retorno no mesmo dia\n");
+    }
+
     printf("Por favor, informe a quantidade de ocupantes desejada: ");
     // https://faq.cprogramming.com/cgi-bin/smartfaq.cgi?answer=1352443831&id=1043284392#:~:text=For%20a%20simple%20solution%2C%20you,with%20the%20other%20format%20specifiers.
     scanf("%d", &vehicleCap);
@@ -40,10 +84,9 @@ void serviceRegisterLocation()
         printf("Não há nenhum veiculo que atenda essa capacidade\n");
         return;
     }
-
     printf("Veiculo %s, %s, %s , %d lugares\n", v.descricao, v.modelo, v.cor, v.qntOcupantes);
 
-    // locationRepo.registerLocation(clientCod, vehicleCod);
+    // locationRepo.registerLocation(clientCod, v.cod);
 }
 
 void serviceListClients()
@@ -54,6 +97,43 @@ void serviceListClients()
 void serviceListVehicles()
 {
     vehicleRepo.listVehicles();
+}
+
+int readDateFromStdin(struct tm *date)
+{
+    const size_t maxStrLength = 100;
+    char dateStr[maxStrLength];
+    fgets(dateStr, maxStrLength, stdin);
+    removeTrailingNewline(dateStr);
+
+    int wdDay, wdMon, wdYear, wdHour, wdMin;
+    if (sscanf(dateStr, "%d/%d/%d %d:%d", &wdDay, &wdMon, &wdYear, &wdHour, &wdMin) != 5)
+        return 0;
+
+    time_t tNow = time(NULL);
+    struct tm tmNow = *localtime(&tNow);
+
+    // Ajusta a data para tm
+    memset(date, 0, sizeof(struct tm));
+    date->tm_mday = wdDay;
+    date->tm_mon = wdMon - 1;
+    date->tm_year = wdYear - 1900;
+    date->tm_hour = wdHour;
+    date->tm_min = wdMin;
+
+    time_t t = mktime(date);
+    if (t == -1)
+        return 0;
+
+    return 1;
+}
+
+int formatDate(struct tm *date, char *buffer, size_t sizeBuffer)
+{
+    if (strftime(buffer, sizeBuffer, "%d/%m/%Y %H:%M", date) > sizeBuffer)
+        return 0;
+    else
+        return 1;
 }
 
 const Service service = {
