462a77de8071b675f8fb0e828dfc6b650d5a255c
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-06-10 11:39:46 -0300

    implementando servico

--- src/main.c
+++ src/main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "client.c"
-#include "vehicle.c"
-#include "location.c"
+#include "service.c"
 
 // programa principal
 int main(void)
---
+++ src/service.c
@@ -0,0 +1,20 @@
+#include "client.c"
+#include "location.c"
+#include "vehicle.c"
+
+// global
+// const char *locationDbFile = "location_db.dat";
+
+// prototypes
+void serviceRegisterLocation();
+// void serviceFinishLocation();
+// void serviceListClients();
+// void serviceListVehicles();
+// void serviceFindByClient(int cod);
+// void serviceFindByVehicle(int cod);
+// void serviceFindLocationsByClient(int cod);
+// void serviceCalcRewardPointsByClient(int cod);
+
+void serviceRegisterLocation()
+{
+}
