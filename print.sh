ac80468c9a9320e8815d8c9440e8acbfdbd5b14d
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-07-02 16:29:05 -0300

    script nome do projeto no terminal

---
+++ print.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+clear
+
+
+# figlet -cf future 'Guilherme Rodrigues' | toilet -f term -F border:gay
+#figlet -ctkf future 'Sistema de Gestao de Locacao (SGL)'
+# figlet -ctkf digital 'Guilherme Rodrigues'
+# figlet -ctkf digital 'Guilherme Rodrigues' | toilet -f term -F metal
+
+#figlet -cf future 'Guilherme Rodrigues' | toilet -f term -F metal
+# figlet -cf future 'Sistema de Gestao de Locacao (SGL)' | toilet -f term -F gay
+toilet -t -f future -F metal 'Sistema de Gestao de Locacoes (SGL)'
+
+# figlet -f term 'Sistema de Gestao de Locacao (SGL)' | toilet -f term -F border:gay
+toilet -t -f term -F border '                                       Guilherme Rodrigues                                       '
+toilet -t -f term -F border '         https://replit.com/@GuilhermeRodri8/trabalho-interdisciplinar-fes-aeds-i                '
+
+# pausa
+read
+
+# ./sgl-locamais
